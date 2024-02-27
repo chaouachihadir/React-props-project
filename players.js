7aa7524533f5f24d3cdd1036cc1abd082e5c61b6
chaouachihadir <chaouachyhadyr@gmail.com>
2024-02-27 17:29:55 +0100

    react props project

---
+++ players.js
@@ -0,0 +1,34 @@
+// players.js
+
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://example.com/messi.jpg",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl: "https://example.com/ronaldo.jpg",
+    },
+    // Add more players as needed
+    <div className="img-container">
+
+                   <img src="messi.jpg" alt="img" />
+                   <img src="cr7" alt="img" />
+
+
+
+                </div>
+               
+  ];
+  
+  
+  export default players;
+  
---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from "react";
+import "./App.css";
+import PlayersList from "./components/PlayersList";
+
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
